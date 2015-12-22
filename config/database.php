fb61596ad5d10a327200660c9b784e5a5b21431d
saifcse06 <saifcse06@gmail.com>
2015-12-22 03:07:42 -0800

    data config

--- config/database.php
+++ config/database.php
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'sqlsrv'),
 
     /*
     |--------------------------------------------------------------------------
@@ -77,10 +77,10 @@ return [
 
         'sqlsrv' => [
             'driver'   => 'sqlsrv',
-            'host'     => env('DB_HOST', 'localhost'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host'     => '103.10.54.99',
+            'database' => 'Digital_School',
+            'username' => 'sa',
+            'password' => 'Qwert3201',
             'charset'  => 'utf8',
             'prefix'   => '',
         ],
