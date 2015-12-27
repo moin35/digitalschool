4916a2de7a79b9357e2d8bc6267d2620c4cf27d8
moin35 <moinmd@yahoo.com>
2015-12-27 19:03:20 +0600

    asd

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Session;
 
@@ -46,6 +47,25 @@ public function getAddStudent(){
 public function postAddStudent(){
     if(Auth::check()){
          if(priv()==1){
+            $name=Input::get('name');
+             $student_id=Input::get('st_id');
+             $address=Input::get('address');
+             $guardian=Input::get('guardian_name');
+             $gender=Input::get('gender');
+             $religion=Input::get('religion');
+             $email=Input::get('email');
+             $phone=Input::get('phone');
+             $class=Input::get('class');
+             $section=Input::get('section');
+             $roll=Input::get('roll');
+             $user_type=Input::get('user_type');
+             $transport_rent=Input::get('transport_rent');
+             $birth_certificate=Input::get('birth_certificate');
+             $image=Input::get('image');
+             $route_name=Input::get('route_name');
+             $status=Input::get('status');
+
+
 
          }
         else{
