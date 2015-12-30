e54c5a412e79c21acbff2d0dcdbfd069389ae08e
saifcse06 <saifcse06@gmail.com>
2015-12-29 17:09:01 -0800

    area add saif

--- resources/views/superadmin/add_search_student.blade.php
+++ resources/views/superadmin/add_search_student.blade.php
@@ -177,31 +177,31 @@
                                                                     <input  type="checkbox" style="width: 20px" class="checkbox form-control" id="newsletter" name="newsletter" />
                                                                 </div>
                                                             </div>
-                                                            <div class="form-group last">
-                                                                <label class="control-label col-md-3">Image Upload</label>
-                                                                <div class="col-md-9">
-                                                                    <div class="fileupload fileupload-new" data-provides="fileupload">
-                                                                        <div class="fileupload-new thumbnail" style="width: 200px; height: 150px;">
-                                                                            <img src="http://www.placehold.it/200x150/EFEFEF/AAAAAA&amp;text=no+image" alt="" />
-                                                                        </div>
-                                                                        <div class="fileupload-preview fileupload-exists thumbnail" style="max-width: 200px; max-height: 150px; line-height: 20px;"></div>
-                                                                        <div>
+                                                             <div class="form-group last">
+                                <label class="control-label col-md-3">Image Upload</label>
+                                <div class="col-md-9">
+                                    <div class="fileupload fileupload-new" data-provides="fileupload">
+                                        <div class="fileupload-new thumbnail" style="width: 200px; height: 150px;">
+                                            <img src="http://www.placehold.it/200x150/EFEFEF/AAAAAA&amp;text=no+image" alt="" />
+                                        </div>
+                                        <div class="fileupload-preview fileupload-exists thumbnail" style="max-width: 200px; max-height: 150px; line-height: 20px;"></div>
+                                        <div>
                                                    <span class="btn btn-white btn-file">
                                                    <span class="fileupload-new"><i class="fa fa-paper-clip"></i> Select image</span>
                                                    <span class="fileupload-exists"><i class="fa fa-undo"></i> Change</span>
                                                    <input type="file" class="default" />
                                                    </span>
-                                                                            <a href="#" class="btn btn-danger fileupload-exists" data-dismiss="fileupload"><i class="fa fa-trash"></i> Remove</a>
-                                                                        </div>
-                                                                    </div>
-                                                                    <span class="label label-danger">NOTE!</span>
+                                            <a href="#" class="btn btn-danger fileupload-exists" data-dismiss="fileupload"><i class="fa fa-trash"></i> Remove</a>
+                                        </div>
+                                    </div>
+                                    <span class="label label-danger">NOTE!</span>
                                              <span>
                                              Attached image thumbnail is
                                              supported in Latest Firefox, Chrome, Opera,
                                              Safari and Internet Explorer 10 only
                                              </span>
-                                                                </div>
-                                                            </div>
+                                </div>
+                            </div>
                                                             <div class="form-group">
                                                                 <div class="col-lg-offset-3 col-lg-6">
                                                                     <button class="btn btn-primary" type="submit">Save</button>
