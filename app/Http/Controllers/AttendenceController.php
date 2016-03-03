ce8e8d9abea8d9d047f07162c7e97010511c94ed
Md. Moin Besuwas <moinmd@yahoo.com>
2016-03-03 12:54:45 +0600

    asert

--- app/Http/Controllers/AttendenceController.php
+++ app/Http/Controllers/AttendenceController.php
@@ -42,15 +42,8 @@ class AttendenceController extends Controller
     {
         $today = date("Y-m-d");
       $iid= Teacher::where('institute_code','=',Auth::user()->institute_id)->pluck('institute_code');
-        
-       $teacher = DB::table('tbl_attendence')
-            ->join('tbl_teacher', 'tbl_attendence.institute_code', '=', 'tbl_teacher.institute_code')
-            ->select('tbl_teacher.teacher_id','tbl_teacher.phone','tbl_teacher.name','tbl_teacher.designation','tbl_teacher.email','tbl_attendence.status')
-            ->where('tbl_attendence.created_at','LIKE',"%$today%")
+        $teacher=Attendence::where('institute_code','=',Auth::user()->institute_id)->where('created_at','LIKE',"%$today%")->get();
 
-          //  ->select('tbl_teacher.teacher_id','tbl_teacher.phone','tbl_teacher.name','tbl_teacher.designation','tbl_teacher.email', 'tbl_attendence.status')
-            ->get();
-          return $teacher;
         return view('admin.attendence.teacherattendence',['teacher'=>$teacher,'p'=>$today,'iid'=>$iid]);
     }
 
@@ -203,17 +196,8 @@ $v=Teacher::where('institute_code','=',$iid)->get();
              $dateTime=Input::get('date');
              $clasId=Input::get('class');
              $sectionName=Input::get('section');
-             $today = date("Y-m-d");
-
-          // $GetStudents=students::where('institute_code','=',Auth::user()->institute_id)->where('class','=',$clasId)->where('section','=',$sectionName)->get();
-            $GetStudents=DB::table('tbl_studets')
-           ->join('tbl_attendence','tbl_studets.st_id','=','tbl_attendence.uid')
-           ->select('tbl_studets.*','tbl_attendence.*')
-           ->where('tbl_attendence.institute_code','=',Auth::user()->institute_id)
-           ->where('tbl_studets.institute_code','=',Auth::user()->institute_id)
-            ->where('tbl_attendence.type','=','Student')
-           ->where('tbl_attendence.created_at','LIKE',"%$today%")
-           ->get();
+           $GetStudents=students::where('institute_code','=',Auth::user()->institute_id)->where('class','=',$clasId)->where('section','=',$sectionName)->get();
+           //$GetStudents=students::where('institute_code','=',Auth::user()->institute_id)->get();
            //   return $GetStudents->count();
            $this->getallclass();
           return view('admin.attendence.studentsAttendence')->with('allclass',$this->getallclass())->with('GetStudents',$GetStudents)
@@ -229,122 +213,34 @@ $v=Teacher::where('institute_code','=',$iid)->get();
       $dateTime=Input::get('date');
       $clasId=Input::get('class');
       $sectionName=Input::get('section');
-      $today = date("Y-m-d");
 
+      $GetStudents=students::where('institute_code','=',Auth::user()->institute_id)->where('class','=',$clasId)->where('section','=',$sectionName)->get();
 
-      $data1=Attendence::where('institute_code','=',Auth::user()->institute_id)
-      ->where('type','=','Student')
-      ->where('created_at','LIKE',"%$today%")
-      ->get();
-      if ($data1->count()==0) {
-        # code...
-        $GetStudents=students::where('institute_code','=',Auth::user()->institute_id)->where('class','=',$clasId)->where('section','=',$sectionName)->get();
+      foreach ($GetStudents as $key => $value) {
 
-        foreach ($GetStudents as $key => $value) {
+      //  return $value->st_id;
 
-        //return $data1->count();
-
-           $stdAtn=new Attendence;
-           $stdAtn->institute_code=Auth::user()->institute_id;
-           $stdAtn->uid=$value->st_id;
-           $stdAtn->type="Student";
-           $stdAtn->status=0;
-           $stdAtn->save();
-
-           //return redirect()->back();
-
-
-        }
-        return view('admin.attendence.studentsAttendence')->with('allclass',$this->getallclass())->with('GetStudents',$GetStudents)
-        ->with('sectionName',$sectionName)->with('classId',$clasId)->with('dateTime',$dateTime);
-        }
-      else {
-        # code...
-        $GetStudents=DB::table('tbl_studets')
-       ->join('tbl_attendence','tbl_studets.st_id','=','tbl_attendence.uid')
-       ->select('tbl_studets.*','tbl_attendence.*')
-       ->where('tbl_attendence.institute_code','=',Auth::user()->institute_id)
-       ->where('tbl_studets.institute_code','=',Auth::user()->institute_id)
-        ->where('tbl_attendence.type','=','Student')
-       ->where('tbl_attendence.created_at','LIKE',"%$today%")
-       ->get();
-
-        return view('admin.attendence.studentsAttendence')->with('allclass',$this->getallclass())->with('GetStudents',$GetStudents)
-        ->with('sectionName',$sectionName)->with('classId',$clasId)->with('dateTime',$dateTime);
-
-
+      $stdAtn=new Attendence;
+      $stdAtn->institute_code=Auth::user()->institute_id;
+      $stdAtn->uid=$value->st_id;
+      $stdAtn->type="Student";
+      $stdAtn->status=0;
+      $stdAtn->save();
 
       }
+
+     return view('admin.attendence.studentsAttendence')->with('allclass',$this->getallclass())->with('GetStudents',$GetStudents)
+     ->with('sectionName',$sectionName)->with('classId',$clasId)->with('dateTime',$dateTime);
+
        //return $GetStudents;
 
     }
+    public function postAllStudentsAttendence(Request $request){
+      if($request->ajax()){
+        $data=Input::all();
+        return $data;
 
-
-    public function StudentsAttendenceforAbsent($uid)
-    {
-      # code...
-      //return $uid;
-        $today = date("Y-m-d");
-      $data1=Attendence::where('institute_code','=',Auth::user()->institute_id)
-      ->where('type','=','Student')
-      ->where('uid','=',$uid)
-      ->where('created_at','LIKE',"%$today%")
-      ->update(['status'=>1]);
-      return redirect()->back();
-
-
-    }
-    public function getStudentsAttendenceIndex(){
-      //$today = date("Y-m-d");
-      $this->getallclass();
-      $clseId=Input::get('class');
-      $sectionId=Input::get('section');
-    /*  $GetStudents=DB::table('tbl_studets')
-     ->join('tbl_attendence','tbl_studets.st_id','=','tbl_attendence.uid')
-     ->select('tbl_studets.*','tbl_attendence.*')
-     ->where('tbl_attendence.institute_code','=',Auth::user()->institute_id)
-     ->where('tbl_studets.institute_code','=',Auth::user()->institute_id)
-      ->where('tbl_attendence.type','=','Student')
-     //->where('tbl_attendence.created_at','LIKE',"%$today%")
-     ->get();*/
-     $GetStudents=students::where('institute_code','=',Auth::user()->institute_id)->where('class','=',$clseId)->where('section','=',$sectionId)->get();
-
-       return view('admin.attendence.studentsAttendenceIndex')->with('allclass',$this->getallclass())->with('GetStudents',$GetStudents);
-    }
-    public function postStudentsAttendenceIndex(){
-      $clseId=Input::get('class');
-      $sectionId=Input::get('section');
-      //return $clseId;
-    //  $today = date("Y-m-d");
-    /*    $this->getallclass();
-      $GetStudents=DB::table('tbl_studets')
-     ->join('tbl_attendence','tbl_studets.st_id','=','tbl_attendence.uid')
-     ->select('tbl_studets.*','tbl_attendence.*')
-     ->where('tbl_attendence.institute_code','=',Auth::user()->institute_id)
-     ->where('tbl_studets.institute_code','=',Auth::user()->institute_id)
-      ->where('tbl_attendence.type','=','Student')
-      ->where('tbl_studets.class','=',$clseId)
-      ->where('tbl_studets.section','=',$sectionId)
-  //   ->where('tbl_attendence.created_at','LIKE',"%$today%")
-     ->get();*/
-     $GetStudents=students::where('institute_code','=',Auth::user()->institute_id)->where('class','=',$clseId)->where('section','=',$sectionId)->get();
-
-       return view('admin.attendence.studentsAttendenceIndex')->with('GetStudents',$GetStudents)->with('allclass',$this->getallclass());
-    }
-    public function postStudentsAttendenceDetails($uid){
-
-      $GetStudents=DB::table('tbl_studets')
-     ->join('tbl_attendence','tbl_studets.st_id','=','tbl_attendence.uid')
-     ->select('tbl_studets.*','tbl_attendence.*')
-     ->where('tbl_attendence.institute_code','=',Auth::user()->institute_id)
-     ->where('tbl_studets.institute_code','=',Auth::user()->institute_id)
-      ->where('tbl_attendence.type','=','Student')
-      ->where('tbl_studets.st_id','=',$uid)
-     ->first();
-         $stdClass=ClassAdd::where('class_id','=',$GetStudents->class)->pluck('class_name');
-      //return $uid;
-      return view('admin.attendence.studentsAttendanceViewDetails')->with('stdInfo',$GetStudents)->with('stdClass',$stdClass);
-
+      }
     }
 
 }
