0873727591fd65eabf2e5a23271e5ed6c579dfaa
saifcse06 <saifcse06@gmail.com>
2016-03-06 13:11:46 +0600

    attandenc student

--- resources/views/admin/attendence/studentsAttendanceViewDetails.blade.php
+++ resources/views/admin/attendence/studentsAttendanceViewDetails.blade.php
@@ -116,23 +116,17 @@ Attendance Information
                                             <tr>
 
                                             <th><?php echo date("F"); ?></th>
-                                                
+
                                                     @foreach ($listdate as $key => $value)
 
-                                              @if($value==date('j'))
-                                              @foreach ($viewAttandence as $key => $value)
-                                                 @if($value->status==0)
+                                                @if(\App\Attendence::where('institute_code','=',Auth::user()->institute_id)->where('uid','=',$stdInfo->st_id)->where('created_at','LIKE',"%$value%")->pluck('status')=='0')
                                                 <td class="att-bg-color"><span class="label label-success label-mini">P</span></td>
-                                                @elseif($value->status==1)
+                                                @elseif(\App\Attendence::where('institute_code','=',Auth::user()->institute_id)->where('uid','=',$stdInfo->st_id)->where('created_at','LIKE',"%$value%")->pluck('status')=='1')
                                                <td class="att-bg-color"><span class="label label-danger label-mini">A</span></td>
-
-
-                                                    @else
-                                                      <td class="att-bg-color"><span class="label label-primary label-mini">F</span></td>
+                                               @else
+                                                  <td class="att-bg-color"><span class="label label-primary label-mini">F</span></td>
                                                      @endif
                                                        @endforeach
-                                                         @endif
-                                                     @endforeach
 
 
                                     </tr>
